45a6b83690af9df58d56929cc7750296e15bb919
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-05-14 00:21:31 -0700

    shell

--- load.sh
+++ load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -n "Enter SuperUser PASSWORD: "; stty -echo; read passwd; stty echo; echo
-cat import.script | sed "s|PASSWORD|$passwd|g" | iris session iris
+cat import.script | sed "s|PASSWORD|$passwd|g" | sed "s|PWD|$(pwd)|g" | iris session iris -U USER
