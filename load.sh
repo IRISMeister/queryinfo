e22d3919fd28d50844a3798266c5ab7ba5869c3b
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2019-10-10 10:57:29 +0900

    loader

--- load.sh
+++ load.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-echo "Will load this program into USER namespace."
+
+instance=iris
+namespace=USER
+if [ $# -ge 1 ]; then
+  instance=$1
+fi
+if [ $# -ge 2 ]; then
+  namespace=$2
+fi
+
+echo "Will load this program into ["$namespace"] namespace of ["$instance"] instance."
 echo -n "Enter SuperUser PASSWORD: "; stty -echo; read passwd; stty echo; echo
-cat load.script | sed "s|PASSWORD|$passwd|g" | sed "s|PWD|$(pwd)|g" | iris session iris -U USER
+cat load.script | sed "s|PASSWORD|$passwd|g" | sed "s|PWD|$(pwd)|g" | iris session $instance -U $namespace
